4bf69836f97784aa18350d46cbded26f358d60ed
Julian Anderson <julian.anderson6207@gmail.com>
2022-11-24 13:27:54 -0500

    ADD neovim config files

---
+++ dot_config/nvim/init.vim
@@ -0,0 +1,27 @@
+" big ole block of set statements
+set nocompatible            " disable compatibility to old-time vi
+set showmatch               " show matching brackets.
+set ignorecase              " case insensitive matching
+set mouse=v                 " middle-click paste with mouse
+set hlsearch                " highlight search results
+set autoindent              " indent a new line the same amount as the line just typed
+set number                  " add line numbers
+set wildmode=longest,list   " get bash-like tab completions
+set cc=88                   " set colour columns for good coding style
+filetype plugin indent on   " allows auto-indenting depending on file type
+set tabstop=4               " number of columns occupied by a tab character
+set expandtab               " convert tabs to white space
+set shiftwidth=4            " width for autoindents
+set softtabstop=4           " see multiple spaces as tabstops so <BS> does the right thing
+set termguicolors
+
+" get all the plugins we need from lua/plugins.lua
+lua require('plugins')
+
+" color scheme settings
+syntax enable
+colorscheme solarized
+
+" mappings/remappings
+let g:mapleader = ','
+nnoremap ; :
---
+++ dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,69 @@
+-- ensures packer is installed if it isn't already
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+    -- manage packer itself
+    use 'wbthomason/packer.nvim'
+
+    -- colorscheme, of course
+    use 'ishan9299/nvim-solarized-lua'
+
+    -- fzf
+    use { 'junegunn/fzf', run = ":call fzf#install()" }
+    use 'junegunn/fzf.vim'
+
+    -- edges
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function() require('gitsigns').setup() end
+    }
+    use {
+        'feline-nvim/feline.nvim',
+        setup = function() vim.opt.termguicolors = true end,
+        config = function() require('feline').setup() end
+    }
+
+    -- treesitter
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+    -- general QoL plugins
+    use 'nvim-tree/nvim-web-devicons'
+    use {
+        'sudormrfbin/cheatsheet.nvim',
+        requires = {
+            {'nvim-telescope/telescope.nvim'},
+            {'nvim-lua/plenary.nvim'},
+        }
+    }
+    use {
+        'ggandor/leap.nvim',
+        requires = {{'tpope/vim-repeat'}},
+        config = function() require('leap').add_default_mappings() end
+    }
+    use {
+        'numToStr/Comment.nvim',
+        config = function() require('Comment').setup() end
+    }
+
+    -- niceties
+    use 'ntpeters/vim-better-whitespace'
+    use 'dodie/vim-disapprove-deep-indentation'
+    use 'chr4/sslsecure.vim'
+    use 'dstein64/vim-startuptime'
+
+    -- for bootstrapping purposes
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)

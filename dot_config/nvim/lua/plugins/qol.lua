7564731995f1a3f6e8304998ea26a060e058a536
Julian Anderson <julian.anderson6207@gmail.com>
2026-01-01 20:05:44 -0500

    nvim: remove rarely (if ever) used plugins

--- dot_config/nvim/lua/plugins/qol.lua
+++ dot_config/nvim/lua/plugins/qol.lua
@@ -1,25 +1,6 @@
 return {
     { "nvim-tree/nvim-web-devicons", lazy = true },
     {
-        "sudormrfbin/cheatsheet.nvim",
-        dependencies = {
-            { "nvim-telescope/telescope.nvim" },
-            { "nvim-lua/plenary.nvim" },
-        }
-    },
-    {
-        "ggandor/leap.nvim",
-        dependencies = { "tpope/vim-repeat" },
-        config = function()
-            local leap = require('leap')
-            vim.keymap.set('n', 's', function ()
-                require('leap').leap({ target_windows = { vim.api.nvim_get_current_win() } })
-            end)
-            vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
-            vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
-        end
-    },
-    {
         "numToStr/Comment.nvim",
         config = function() require("Comment").setup() end,
     },
